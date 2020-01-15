beaa85497be0fa45e20eb7379f242ca1b827ce82
Louis Hamilton <louis@iastate.edu>
2020-01-14 19:12:54 -0600

    First commit, index js runs and testing on the root page clears

---
+++ tests/api.js
@@ -0,0 +1,24 @@
+// Import the dependencies for testing
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const Mongoose = require('mongoose');
+var should = chai.should();
+var db;
+
+
+chai.use(chaiHttp);
+var app = require('../index');
+
+describe("Testing the Server", function() {
+  //Testing access to the index page
+  it('should get the root page on / GET', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res){
+        should.exist(res);
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+});
